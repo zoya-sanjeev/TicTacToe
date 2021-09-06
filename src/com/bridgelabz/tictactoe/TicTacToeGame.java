c46f1b04eab3246dcfeb64dfbf45e9bfabd38eec
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-09-06 08:48:03 +0530

    ADDED: playerInput method to take input of player

--- src/com/bridgelabz/tictactoe/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoe/TicTacToeGame.java
@@ -1,18 +1,24 @@
 package com.bridgelabz.tictactoe;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
-	
+	static Scanner scanner =new Scanner(System.in);
 	static char[] board=new char[10];
 	
 	public static void assignSpace() {
 		for(int index=1;index<=board.length;index++)
 			board[index]=' ';
-		
+	}
+	
+	public static void playerInput() {
+		System.out.println("Enter Player move");
+		char playerMove=scanner.next().charAt(0);
 	}
 
 	public static void main(String[] args) {
-		
 		assignSpace();
+		playerInput();
 
 	}
 
