e8f4a0b9fad4fa0dc38bc43260788c1c886625d4
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-09-06 08:21:37 +0530

    ADDED: assignSpace method added to initialize board array

--- src/com/bridgelabz/tictactoe/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoe/TicTacToeGame.java
@@ -1,9 +1,16 @@
 package com.bridgelabz.tictactoe;
 
 public class TicTacToeGame {
+	
+	public static void assignSpace(char[] board) {
+		for(int index=1;index<=board.length;index++)
+			board[index]=' ';
+		
+	}
 
 	public static void main(String[] args) {
-		
+		char[] board=new char[10];
+		assignSpace(board);
 
 	}
 
